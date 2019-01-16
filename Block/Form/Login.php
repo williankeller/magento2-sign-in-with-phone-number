9fae641dce091ead81e25e49604ef511ffa170dc
Willian Keller <wkeller@ciandt.com>
2019-01-16 14:28:28 -0200

    Improve block doc

--- Block/Form/Login.php
+++ Block/Form/Login.php
@@ -1,8 +1,15 @@
 <?php
+
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * A Magento 2 module named Magestat/SigninPhoneNumber
+ * Copyright (C) 2019 Magestat
+ *
+ * This file included in Magestat/SigninWithPhoneNumber is licensed under OSL 3.0
+ *
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
  */
+
 namespace Magestat\SigninPhoneNumber\Block\Form;
 
 use Magento\Framework\View\Element\Template\Context;
