cbb0fa590d8039463ff69e15ab7dc53181872737
Willian Keller <wkeller@ciandt.com>
2019-01-17 16:22:50 -0200

    Add customer attribute

---
+++ Setup/InstallData.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * A Magento 2 module named Magestat/SigninPhoneNumber
+ * Copyright (C) 2019 Magestat
+ *
+ * This file included in Magestat/SigninWithPhoneNumber is licensed under OSL 3.0
+ *
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Magestat\SigninPhoneNumber\Setup;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var \Magento\Customer\Setup\CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $setup->startSetup();
+
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerSetup->addAttribute(
+            Customer::ENTITY,
+            'phone_number', [
+                'type' => 'static',
+                'label' => 'Phone Number',
+                'input' => 'text',
+                'required' => false,
+                'sort_order' => 200,
+                'visible' => true,
+                'system' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => true,
+                'is_filterable_in_grid' => true,
+                'is_searchable_in_grid' => true
+            ]
+        );
+        /** @var $attribute */
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'customer_number');
+        $attribute->setData('used_in_forms', ['adminhtml_customer']);
+        $attribute->save();
+
+        $setup->endSetup();
+    }
+}
