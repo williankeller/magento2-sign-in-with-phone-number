51deb9bf2d40bc449d7f139b8c309d2eebdae216
Willian Keller <wkeller@ciandt.com>
2019-01-25 10:15:31 -0200

    Override AccountManagement class

---
+++ Model/AccountManagement.php
@@ -0,0 +1,217 @@
+<?php
+
+/**
+ * A Magento 2 module named Magestat/SigninPhoneNumber
+ * Copyright (C) 2019 Magestat
+ *
+ * This file included in Magestat/SigninWithPhoneNumber is licensed under OSL 3.0
+ *
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Magestat\SigninPhoneNumber\Model;
+
+use Magento\Customer\Api\AddressRepositoryInterface;
+use Magento\Customer\Api\CustomerMetadataInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\ValidationResultsInterfaceFactory;
+use Magento\Customer\Helper\View as CustomerViewHelper;
+use Magento\Customer\Model\Config\Share as ConfigShare;
+use Magento\Customer\Model\Customer as CustomerModel;
+use Magento\Customer\Model\CustomerFactory;
+use Magento\Customer\Model\CustomerRegistry;
+use Magento\Customer\Model\Metadata\Validator;
+use Magento\Framework\Api\ExtensibleDataObjectConverter;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\DataObjectFactory as ObjectFactory;
+use Magento\Framework\Encryption\EncryptorInterface as Encryptor;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Exception\EmailNotConfirmedException;
+use Magento\Framework\Exception\InvalidEmailOrPasswordException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\Math\Random;
+use Magento\Framework\Reflection\DataObjectProcessor;
+use Magento\Framework\Registry;
+use Magento\Framework\Stdlib\DateTime;
+use Magento\Framework\Stdlib\StringUtils as StringHelper;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface as PsrLogger;
+use Magestat\SigninPhoneNumber\Model\Config\Source\SigninMode;
+use Magestat\SigninPhoneNumber\Model\Handler\Signin as HandlerSignin;
+
+/**
+ * Class AccountManagement
+ *
+ * @package \Magestat\SigninPhoneNumber\Model
+ * @see \Magento\Customer\Model\AccountManagement
+ */
+class AccountManagement extends \Magento\Customer\Model\AccountManagement
+{
+    /**
+     * @var \Magento\Customer\Api\CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
+     * @var \Magento\Customer\Model\CustomerFactory
+     */
+    private $customerFactory;
+
+    /**
+     * @var \Magento\Framework\Event\ManagerInterface
+     */
+    private $eventManager;
+
+    /**
+     * @var \Magestat\SigninPhoneNumber\Model\Handler\Signin
+     */
+    protected $handlerSignin;
+
+    /**
+     * @param CustomerFactory $customerFactory
+     * @param ManagerInterface $eventManager
+     * @param StoreManagerInterface $storeManager
+     * @param Random $mathRandom
+     * @param Validator $validator
+     * @param ValidationResultsInterfaceFactory $validationResultsDataFactory
+     * @param AddressRepositoryInterface $addressRepository
+     * @param CustomerMetadataInterface $customerMetadataService
+     * @param CustomerRegistry $customerRegistry
+     * @param PsrLogger $logger
+     * @param Encryptor $encryptor
+     * @param ConfigShare $configShare
+     * @param StringHelper $stringHelper
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param ScopeConfigInterface $scopeConfig
+     * @param TransportBuilder $transportBuilder
+     * @param DataObjectProcessor $dataProcessor
+     * @param Registry $registry
+     * @param CustomerViewHelper $customerViewHelper
+     * @param DateTime $dateTime
+     * @param CustomerModel $customerModel
+     * @param ObjectFactory $objectFactory
+     * @param ExtensibleDataObjectConverter $extensibleDataObjectConverter
+     * @param HandlerSignin $handlerSignin
+     */
+    public function __construct(
+        CustomerFactory $customerFactory,
+        ManagerInterface $eventManager,
+        StoreManagerInterface $storeManager,
+        Random $mathRandom,
+        Validator $validator,
+        ValidationResultsInterfaceFactory $validationResultsDataFactory,
+        AddressRepositoryInterface $addressRepository,
+        CustomerMetadataInterface $customerMetadataService,
+        CustomerRegistry $customerRegistry,
+        PsrLogger $logger,
+        Encryptor $encryptor,
+        ConfigShare $configShare,
+        StringHelper $stringHelper,
+        CustomerRepositoryInterface $customerRepository,
+        ScopeConfigInterface $scopeConfig,
+        TransportBuilder $transportBuilder,
+        DataObjectProcessor $dataProcessor,
+        Registry $registry,
+        CustomerViewHelper $customerViewHelper,
+        DateTime $dateTime,
+        CustomerModel $customerModel,
+        ObjectFactory $objectFactory,
+        ExtensibleDataObjectConverter $extensibleDataObjectConverter,
+        HandlerSignin $handlerSignin
+    ) {
+        parent::__construct(
+            $customerFactory,
+            $eventManager,
+            $storeManager,
+            $mathRandom,
+            $validator,
+            $validationResultsDataFactory,
+            $addressRepository,
+            $customerMetadataService,
+            $customerRegistry,
+            $logger,
+            $encryptor,
+            $configShare,
+            $stringHelper,
+            $customerRepository,
+            $scopeConfig,
+            $transportBuilder,
+            $dataProcessor,
+            $registry,
+            $customerViewHelper,
+            $dateTime,
+            $customerModel,
+            $objectFactory,
+            $extensibleDataObjectConverter
+        );
+        $this->customerRepository = $customerRepository;
+        $this->customerFactory = $customerFactory;
+        $this->eventManager = $eventManager;
+        $this->handlerSignin = $handlerSignin;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function authenticate($username, $password)
+    {
+        try {
+            $customer = $this->customerRepository->get($username);
+        } catch (NoSuchEntityException $e) {
+            throw new InvalidEmailOrPasswordException(__('Invalid login or password.'));
+        }
+
+        $customerId = $customer->getId();
+        if ($this->getAuthentication()->isLocked($customerId)) {
+            throw new UserLockedException(__('The account is locked.'));
+        }
+        try {
+            $this->getAuthentication()->authenticate($customerId, $password);
+        } catch (InvalidEmailOrPasswordException $e) {
+            throw new InvalidEmailOrPasswordException(__('Invalid login or password.'));
+        }
+        if ($customer->getConfirmation() && $this->isConfirmationRequired($customer)) {
+            throw new EmailNotConfirmedException(__("This account isn't confirmed. Verify and try again."));
+        }
+
+        $this->dispatchEvents($customer, $password);
+
+        return $customer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    private function getAuthentication()
+    {
+        if (!($this->authentication instanceof AuthenticationInterface)) {
+            return \Magento\Framework\App\ObjectManager::getInstance()->get(
+                \Magento\Customer\Model\AuthenticationInterface::class
+            );
+        } else {
+            return $this->authentication;
+        }
+    }
+    
+    /**
+     * @param object $customer Customer object.
+     * @param string $password Customer password.
+     * @return object \Magestat\SigninPhoneNumber\Model\AccountManagement
+     */
+    private function dispatchEvents($customer, $password)
+    {
+        $customerModel = $this->customerFactory->create()->updateData($customer);
+        $this->eventManager->dispatch(
+            'customer_customer_authenticated',
+            ['model' => $customerModel, 'password' => $password]
+        );
+
+        $this->eventManager->dispatch(
+            'customer_data_object_login',
+            ['customer' => $customer]
+        );
+        return $this;
+    }
+}
---
+++ Model/Handler/Signin.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * A Magento 2 module named Magestat/SigninPhoneNumber
+ * Copyright (C) 2019 Magestat
+ *
+ * This file included in Magestat/SigninWithPhoneNumber is licensed under OSL 3.0
+ *
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Magestat\SigninPhoneNumber\Model\Handler;
+
+class Signin
+{
+
+}
